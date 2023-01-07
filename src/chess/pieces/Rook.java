2b101dd001efabeffaec95585a76a4bb3aa97c78
Lucas Bicalho <bicalho.lucas@gmail.com>
2023-01-07 17:12:14 -0300

    Handling captured pieces

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -43,7 +43,7 @@ public class Rook extends ChessPiece {
 		}
 		
 		//right
-		p.setValues(position.getRow(), position.getColumn() - +1);
+		p.setValues(position.getRow(), position.getColumn() + 1);
 		while (getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
 			mat[p.getRow()][p.getColumn()] = true;
 			p.setColumn(p.getColumn() +1);
